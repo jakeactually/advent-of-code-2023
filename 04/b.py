1dda35d45b4c6da17480da3933e8f69a71750124
Jeyko Caicedo <jakeactually@outlook.com>
2026-04-21 17:49:55 -0500

    day 04 b

--- 04/b.py
+++ 04/b.py
@@ -2,13 +2,22 @@ import re
 
 with open('input.txt') as input:
     total = 0
+    cache = {}
 
     for line in input:
         digits = re.findall(r'\d+', line)
         winning = digits[1:11]
         candidates = digits[11:]
         common = set(winning) & set(candidates)
-        value = pow(2, len(common) - 1) if common else 0
-        total += value
+        cache[digits[0]] = len(common)
 
-    print(total)
+    def get_value(key):
+        if key in cache:
+            start = int(key) + 1
+            end = start + cache[key]
+            return 1 + sum(
+                get_value(f'{k}') for k in range(start, end)
+            )
+        return 0
+    
+    print(sum(get_value(k) for k in cache))
