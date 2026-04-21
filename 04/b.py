926bd623f9d2c1cee9037a966dbc7eed7116a3df
Jeyko Caicedo <jakeactually@outlook.com>
2026-04-21 15:51:59 -0500

    day 4 b setup

--- 04/b.py
+++ 04/b.py
@@ -1,2 +1,14 @@
+import re
+
 with open('input.txt') as input:
-    print(input)
+    total = 0
+
+    for line in input:
+        digits = re.findall(r'\d+', line)
+        winning = digits[1:11]
+        candidates = digits[11:]
+        common = set(winning) & set(candidates)
+        value = pow(2, len(common) - 1) if common else 0
+        total += value
+
+    print(total)
