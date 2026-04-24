de7ef4d786c83d1b4ca69ff9de0e50a9085642b5
Jeyko Caicedo <jakeactually@outlook.com>
2026-04-23 22:21:48 -0500

    day 5 a

--- 05/a.py
+++ 05/a.py
@@ -1,4 +1,33 @@
 import re
 
 with open('input.txt') as input:
-    print(0)
+    lines = input.readlines()
+    seeds = re.findall(r'\d+', lines[0])
+    ranges = []
+
+    for line in lines[1:]:
+        if line.strip() == '':
+            continue
+
+        if 'map' in line:
+            ranges.append([])
+            continue
+
+        ranges[-1].append(
+            [int(n) for n in re.findall(r'\d+', line)]
+        )
+    
+    locations = []
+
+    for seed in seeds:
+        current = int(seed)
+
+        for rangelist in ranges:
+            for (dst, src, length) in rangelist:
+                if current in range(src, src + length):
+                    current = dst + (current - src)
+                    break
+        
+        locations.append(current)
+
+    print(min(locations))
