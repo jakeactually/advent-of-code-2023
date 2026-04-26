361568bbfaaed2e4fb8126ab896aa6d373e8acda
Jeyko Caicedo <jakeactually@outlook.com>
2026-04-25 20:00:01 -0500

    day 6 b setup

--- 06/b.py
+++ 06/b.py
@@ -1,5 +1,13 @@
-import re
+import pandas as pd
 
 with open('input.txt') as input:
+    time_distance = pd.read_table(input, index_col=0, header=None, sep=r'\s+')
 
-    print(0)
+    for label, content in time_distance.items():
+        time = content['Time:']
+        distance = content['Distance:']
+
+        for i in range(time):
+            if i * (time - i) > distance:
+                print(time + 1 - i * 2)
+                break
