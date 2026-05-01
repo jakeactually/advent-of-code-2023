eec486b1b9445ba21a5dd786895fed31e5451ea7
Jeyko Caicedo <jakeactually@outlook.com>
2026-04-30 23:25:15 -0500

    day 9 a

--- 09/a.py
+++ 09/a.py
@@ -2,4 +2,26 @@
 import re
 
 with open('input.txt') as input:
-    print(input)
+    total = 0
+
+    for line in input:
+        series = [int(x) for x in re.findall(r'-?\d+', line)]
+        stack = []
+
+        while any(series):
+            next_series = []
+
+            for i in range(len(series) - 1):
+                next_series.append(series[i + 1] - series[i])
+
+            stack.append(series)
+            series = next_series
+        
+        for i in reversed(range(len(stack) - 1)):
+            stack[i].append(
+                stack[i][-1] + stack[i + 1][-1]
+            )
+
+        total += stack[0][-1]
+    
+    print(total)
