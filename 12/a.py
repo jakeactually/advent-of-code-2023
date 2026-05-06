f7aaa3f5d21e4bbe5f98f35256078c42fe66e989
Jeyko Caicedo <jakeactually@outlook.com>
2026-05-06 18:53:58 -0500

    day 12 a first try

--- 12/a.py
+++ 12/a.py
@@ -1,5 +1,34 @@
 
 import re
 
+def count_groups(chars):
+    count = 0
+
+    for char in chars:
+        if char == '#':
+            count += 1
+        else:
+            yield count
+            count = 0
+
+    yield count
+
+def validate(springs, counts):
+    for group, count in zip(count_groups(springs), counts):
+        if group != count:
+            return False
+
+    return True
+
+def multiplex(springs):
+    [x, *xs] = springs
+
+    match x:
+        case '?':
+            return (['.', *xs], ['#', *xs])
+
 with open('input.txt') as input:
-    print(input)
+    springs = list('???.###')
+    counts = [1,1,3]
+
+    print(validate(list('##.##.###'), [2,2]))
