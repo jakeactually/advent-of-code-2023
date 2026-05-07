aae18d3a325ff478c92b8c241e1658931bf818e3
Jeyko Caicedo <jakeactually@outlook.com>
2026-05-06 20:25:56 -0500

    day 12 second try

--- 12/a.py
+++ 12/a.py
@@ -1,34 +1,30 @@
 
 import re
 
-def count_groups(chars):
-    count = 0
+def out(springs, in_group, groups, counts):
+    if not springs or not counts:
+        print(groups, springs, counts)
+        return
 
-    for char in chars:
-        if char == '#':
-            count += 1
-        else:
-            yield count
-            count = 0
-
-    yield count
-
-def validate(springs, counts):
-    for group, count in zip(count_groups(springs), counts):
-        if group != count:
-            return False
-
-    return True
-
-def multiplex(springs):
     [x, *xs] = springs
 
     match x:
+        case '#':
+            [*gs, g] = groups if in_group else [*groups, 0]
+            if g < counts[0]:
+                out(xs, True, [*gs, g + 1], counts)
+        case '.':
+            if in_group:
+                if groups[-1] == counts[0]:
+                    out(xs, False, groups + [], counts[1:])
+            else:
+                out(xs, False, groups.copy(), counts)
         case '?':
-            return (['.', *xs], ['#', *xs])
+            out(['.', *xs], in_group, groups.copy(), counts)
+            out(['#', *xs], in_group, groups.copy(), counts)
 
-with open('input.txt') as input:
-    springs = list('???.###')
-    counts = [1,1,3]
+with open('12/input.txt') as input:
+    springs = list('?###????????')
+    counts = [3,2,1]
 
-    print(validate(list('##.##.###'), [2,2]))
+    out(springs, False, [], counts)
