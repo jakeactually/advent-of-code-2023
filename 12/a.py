5c1667752bec2fb0097412680f2855cfed291caf
Jeyko Caicedo <jakeactually@outlook.com>
2026-05-06 22:54:15 -0500

    Revert "day 12 second try"
    This reverts commit aae18d3a325ff478c92b8c241e1658931bf818e3.

--- 12/a.py
+++ 12/a.py
@@ -1,30 +1,34 @@
 
 import re
 
-def out(springs, in_group, groups, counts):
-    if not springs or not counts:
-        print(groups, springs, counts)
-        return
+def count_groups(chars):
+    count = 0
 
+    for char in chars:
+        if char == '#':
+            count += 1
+        else:
+            yield count
+            count = 0
+
+    yield count
+
+def validate(springs, counts):
+    for group, count in zip(count_groups(springs), counts):
+        if group != count:
+            return False
+
+    return True
+
+def multiplex(springs):
     [x, *xs] = springs
 
     match x:
-        case '#':
-            [*gs, g] = groups if in_group else [*groups, 0]
-            if g < counts[0]:
-                out(xs, True, [*gs, g + 1], counts)
-        case '.':
-            if in_group:
-                if groups[-1] == counts[0]:
-                    out(xs, False, groups + [], counts[1:])
-            else:
-                out(xs, False, groups.copy(), counts)
         case '?':
-            out(['.', *xs], in_group, groups.copy(), counts)
-            out(['#', *xs], in_group, groups.copy(), counts)
+            return (['.', *xs], ['#', *xs])
 
-with open('12/input.txt') as input:
-    springs = list('?###????????')
-    counts = [3,2,1]
+with open('input.txt') as input:
+    springs = list('???.###')
+    counts = [1,1,3]
 
-    out(springs, False, [], counts)
+    print(validate(list('##.##.###'), [2,2]))
