ca0216c80dda0f949a899d1bd699938c83d57fe6
Jeyko Caicedo <jakeactually@outlook.com>
2026-05-07 16:55:17 -0500

    day 12 try 3

--- 12/b.py
+++ 12/b.py
@@ -1,20 +1,33 @@
 with open('input.txt') as input:
+    lines = input.read().splitlines()
+    sorted_lines = sorted(lines, key = lambda l: l.count('?'))
     total = 0
 
-    for line in input:
+    for line in sorted_lines:
         [springs, counts] = line.strip().split(' ')
-        list_springs = list(springs)
-        int_counts = [int(i) for i in counts.split(',')]
+        list_springs = ([*springs, '?'] * 5)[:-1]
+        int_counts = [int(i) for i in counts.split(',')] * 5
         
         stack = [([], False, list_springs)]
+        visited = set()
 
         while stack:
             (groups, in_group, springs) = stack.pop()
+
+            if f'{groups} ' + ''.join(springs) in set():
+                continue
+
+            visited.add(f'{groups} ' + ''.join(springs))
 
             if not springs:
                 if groups == int_counts:
                     total += 1
                 continue
+
+            if len(springs) < sum(int_counts) - sum(groups):
+                continue
+
+            print(groups, springs)
 
             [x, *xs] = springs
 
