c7728dddabd9959a42f49e23751f421fa55c6042
Jeyko Caicedo <jakeactually@outlook.com>
2026-05-08 13:25:18 -0500

    day 12 b, I copied the answer

--- 12/b.py
+++ 12/b.py
@@ -1,56 +1,39 @@
-with open('12/input.txt') as input:
+# https://www.reddit.com/r/adventofcode/comments/18ge41g/comment/kefgbn7/
+
+import re
+from functools import lru_cache
+
+spring_regex = re.compile(r'#+')
+springs_regex = re.compile(r'#+\.')
+
+@lru_cache(maxsize=None)
+def count_poss(pos, hints):
+    if '?' not in pos:
+        return tuple(len(s) for s in spring_regex.findall(pos)) == hints
+    
+    i = pos.index('?')
+    ranges = [(m.start(), m.end()) for m in springs_regex.finditer(pos[:i + 1])]
+    lr = len(ranges)
+    diffs = [e - s - 1 for s, e in ranges]
+
+    if len(hints) < lr or any(d != h for d, h in zip(diffs, hints)):
+        return 0
+
+    j = 0 if lr == 0 else ranges[-1][1]
+    new_hints = hints[lr:]    
+    before, after = pos[j : i], pos[i + 1:]
+
+    return sum([
+        count_poss((before + '.' + after).strip('.'), new_hints),
+        count_poss((before + '#' + after).strip('.'), new_hints),
+    ])
+    
+with open('input.txt') as input:
     total = 0
 
     for line in input:
         [springs, counts] = line.strip().split(' ')
-        list_springs = ([*springs, '?'] * 5)[:-1]
-        int_counts = [int(i) for i in counts.split(',')] * 5
-        
-        stack = [([], [], int_counts)]
-        cache = set()
-
-        while stack:
-            positions, groups, counts = stack.pop()
-
-            if f'{positions}' in cache:
-                total += 1
-                continue
-
-            if not counts:
-                ranges = [range(p, p + g) for p, g in zip(positions, groups)]
-                valid = True
-
-                for i, s in enumerate(list_springs):
-                    if s == '#' and not any(i in r for r in ranges):
-                        valid = False
-                
-                if valid:
-                    total += 1
-                    cache.add(f'{positions}')
-                
-                continue
-
-            [c, *cs] = counts
-            
-            start = positions[-1] + groups[-1] + 1 if positions else 0
-            
-            for i in range(start, len(list_springs) - sum(cs) - len(cs) + 1 - c):
-                valid = True
-                
-                for j in range(i, i + c):
-                    if list_springs[j] == '.':
-                        valid = False
-
-                if (i + c) < len(list_springs) and list_springs[i + c] == '#':
-                    valid = False
-
-                if i > 0 and list_springs[i - 1] == '#':
-                    valid = False
-
-                if list_springs[i + c:].count('#') > sum(cs):
-                    valid = False
-
-                if valid:
-                    stack.append(([*positions, i], [*groups, c], cs))
+        int_counts = tuple(int(i) for i in counts.split(',')) * 5
+        total += count_poss('?'.join([springs] * 5), int_counts)
 
     print(total)
