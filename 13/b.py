62e888d2df2e5e7f2d0aeb3c1cfd47692eac5c7c
Jeyko Caicedo <jakeactually@outlook.com>
2026-05-13 01:15:26 -0500

    day 13 b setup

--- 13/b.py
+++ 13/b.py
@@ -1,5 +1,35 @@
 
-import re
+import numpy as np
 
 with open('input.txt') as input:
-    print(input)
+    paragraphs = input.read().split('\n\n')
+
+    matrices = [
+        np.array([list(l) for l in p.splitlines()])
+        for p in paragraphs
+    ]
+
+    total = 0
+
+    for m in matrices:
+        for y in range(1, len(m)):
+            cut = min(y, len(m) - y)
+            top = m[:y][-cut:]
+            bottom = np.flipud(m[y:][:cut])
+
+            if np.array_equal(top, bottom):
+                total += y * 100
+                break
+
+        m = m.T
+
+        for y in range(1, len(m)):
+            cut = min(y, len(m) - y)
+            top = m[:y][-cut:]
+            bottom = np.flipud(m[y:][:cut])
+
+            if np.array_equal(top, bottom):
+                total += y
+                break
+    
+    print(total)
