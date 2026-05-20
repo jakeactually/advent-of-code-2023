8af247fb38b76b330c1ac692e61a89491eb2898b
Jeyko Caicedo <jakeactually@outlook.com>
2026-05-20 18:19:13 -0500

    day 15 a

--- scaffold.py
+++ scaffold.py
@@ -14,7 +14,7 @@ folder = day.zfill(2)
 os.makedirs(folder)
 
 for part in (1, 2):
-    content = TEMPLATE
+    content = TEMPLATE.strip()
     with open(os.path.join(folder, 'a.py' if part == 1 else 'b.py'), 'w') as f:
         f.write(content)
 
