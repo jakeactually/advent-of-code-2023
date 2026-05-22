d13d02c4274920d7be72c2a99a9a6765f859f7b2
Jeyko Caicedo <jakeactually@outlook.com>
2026-05-22 12:18:27 -0500

    day 16 b

--- 16/b.py
+++ 16/b.py
@@ -37,23 +37,33 @@ with open('input.txt') as input:
     height = len(matrix)
     width = len(matrix[0])
 
-    beams = [(0, 0, 'right')]
-    visited = set()
+    def energized(x, y, direction):
+        beams = [(x, y, direction)]
+        visited = set()
 
-    while beams:
-        x, y, direction = beams.pop()
+        while beams:
+            x, y, direction = beams.pop()
 
-        if (x, y, direction) in visited:
-            continue
+            if (x, y, direction) in visited:
+                continue
 
-        if not (0 <= x < width and 0 <= y < height):
-            continue
+            if not (0 <= x < width and 0 <= y < height):
+                continue
 
-        for nd in next_directions(direction, matrix[y][x]):
-            dx, dy = direction_deltas[nd]
-            beams.append((x + dx, y + dy, nd))
-        
-        visited.add((x, y, direction))
+            for nd in next_directions(direction, matrix[y][x]):
+                dx, dy = direction_deltas[nd]
+                beams.append((x + dx, y + dy, nd))
+            
+            visited.add((x, y, direction))
 
-    coords = set((x, y) for x, y, _ in visited)
-    print(len(coords))
+        return set((x, y) for x, y, _ in visited)
+    
+    top_edge = [(x, 0, 'down') for x in range(width)]
+    right_edge = [(width - 1, y, 'left') for y in range(height)]
+    bottom_edge = [(x, height - 1, 'up') for x in range(width)]
+    left_edge = [(0, y, 'right') for y in range(height)]
+    all_edges = top_edge + right_edge + bottom_edge + left_edge
+
+    print(max(
+        len(energized(x, y, direction)) for x, y, direction in all_edges
+    ))
