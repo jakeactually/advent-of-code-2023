4f72fd2d1f5225e6ab4723957020362840973c1c
Jeyko Caicedo <jakeactually@outlook.com>
2026-06-05 12:26:13 -0500

    day 18 a shoelace

--- 18/a.py
+++ 18/a.py
@@ -5,38 +5,34 @@ direction_to_delta = {
     'D': (0, 1)
 }
 
+def shoelace(points):
+    """
+    Compute the area of a polygon using the Shoelace formula.
+
+    points: list of (x, y) tuples in polygon order
+    """
+    n = len(points)
+    area = 0
+
+    for i in range(n):
+        x1, y1 = points[i]
+        x2, y2 = points[(i + 1) % n]  # wrap around to first point
+
+        area += x1 * y2
+        area -= y1 * x2
+
+    return abs(area) / 2
+
 with open('input.txt') as input:
     steps = [line.split() for line in input.read().splitlines()]
-    points = set()
+    points = []
     current = (0, 0)
+    perimeter = 0
 
     for (dir, size, _) in steps:
         (ox, oy) = direction_to_delta[dir]
+        points.append(current)
+        current = (current[0] + int(size) * ox, current[1] + int(size) * oy)
+        perimeter += int(size)
 
-        for j in range(int(size)):
-            points.add(current)
-            current = (current[0] + ox, current[1] + oy)
-
-    start_x = min(x for (x, _) in points) - 1
-    start_y = min(y for (_, y) in points) - 1
-    end_x = max(x for (x, _) in points) + 1
-    end_y = max(y for (_, y) in points) + 1
-    
-    queue = [(start_x, start_y)]
-    visited = set(queue)
-
-    while queue:
-        current = queue.pop(0)
-
-        for (ox, oy) in direction_to_delta.values():
-            next = (current[0] + ox, current[1] + oy)
-
-            if next not in points and start_x <= next[0] <= end_x and start_y <= next[1] <= end_y:
-                if next not in visited:
-                    queue.append(next)            
-                visited.add(next)
-
-    width = end_x - start_x
-    height = end_y - start_y
-    
-    print(height * width - (len(visited) - width - height - 1))
+    print(shoelace(points) + perimeter // 2 + 1)
