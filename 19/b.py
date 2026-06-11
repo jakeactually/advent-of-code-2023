e6d9af093e1bed09eb92748060212b0e3ce059e7
Jeyko Caicedo <jakeactually@outlook.com>
2026-06-10 19:58:24 -0500

    day 19 b

--- 19/b.py
+++ 19/b.py
@@ -1,3 +1,15 @@
+def invert_cond_and_limit(cond, limit):
+    if cond[1] == '>':
+        return (cond[0] + '<', limit + 1)
+    else:
+        return (cond[0] + '>', limit - 1)
+
+def before_or_after(cond, before, after):
+    if '>' in cond:
+        return max(before, after)
+    else:
+        return min(before, after)
+
 with open('input.txt') as input:
     flows_text, parts_text = input.read().split('\n\n')
     flows = {}
@@ -20,42 +32,77 @@ with open('input.txt') as input:
 
     accepted_parts = []
 
-    for part in parts:
-        current_flow_key = 'in'
-        complete = False
+    pending_parts = [{
+        'current_flow_key': 'in',
+        'current_flow_index': 0,
+        'x>': 0,
+        'x<': 4001,
+        'm>': 0,
+        'm<': 4001,
+        'a>': 0,
+        'a<': 4001,
+        's>': 0,
+        's<': 4001,
+    }]
 
-        x = part['x']
-        m = part['m']
-        a = part['a']
-        s = part['s']
+    while pending_parts:
+        for p in pending_parts:
+            print(p)
+        print()
 
-        while not complete:
-            current_flow = flows.get(current_flow_key, [current_flow_key])
+        current_part = pending_parts.pop()       
+        current_flow_key = current_part['current_flow_key']
+        current_flow = flows[current_flow_key]
+        step = current_flow[current_part['current_flow_index']]
 
-            for step in current_flow:
-                if step == 'A':
-                    accepted_parts.append(part)
-                    complete = True
-                    break
+        if step == 'A':
+            accepted_parts.append(current_part)
+            continue
 
-                if step == 'R':
-                    complete = True
-                    break
+        if step == 'R':
+            continue
 
-                if ':' not in step:
-                    current_flow_key = step
-                    break
+        if ':' not in step:
+            positive = current_part.copy()
+            positive['current_flow_key'] = step
+            positive['current_flow_index'] = 0
+            pending_parts.append(positive)
+            continue
 
-                exp, next_flow_key = step.split(':')
+        exp, next_flow_key = step.split(':')
+        cond = exp[:2]
+        limit = int(exp[2:])
 
-                if eval(exp):
-                    current_flow_key = next_flow_key
-                    break
+        positive = current_part.copy()
+
+        if next_flow_key != 'R':
+            positive[cond] = before_or_after(cond, positive[cond], limit)
+
+            if next_flow_key == 'A':
+                accepted_parts.append(positive)
+            else:
+                positive['current_flow_key'] = next_flow_key
+                positive['current_flow_index'] = 0
+                pending_parts.append(positive)
+
+        negative = current_part.copy()
+        next_cond, next_limit = invert_cond_and_limit(cond, limit)
+        negative[next_cond] = before_or_after(next_cond, negative[next_cond], next_limit)
+        negative['current_flow_index'] += 1
+        pending_parts.append(negative)
 
     total = 0
 
-    for part in accepted_parts:
-        for value in part.values():
-            total += value
+    for p in accepted_parts:
+        print(p)
+        product = 1
 
-    print(total)
+        for c in 'xmas':
+            mult = p[c + '<'] - p[c + '>'] - 1
+            print(c, mult)
+            product *= mult
+
+        print('product', product)
+        total += product
+
+    print('total', total)
