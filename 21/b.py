011c4754a3ebc2f1b9bb813df68f55c8598b5c02
Jeyko Caicedo <jakeactually@outlook.com>
2026-06-17 13:21:35 -0500

    day 21 b simplified some parts for me

--- 21/b.py
+++ 21/b.py
@@ -1,62 +1,13 @@
 # https://www.reddit.com/r/adventofcode/comments/18nevo3/comment/l02tjtl/
 
 from collections import deque
-from itertools import product
-from operator import itemgetter
 
-OFFSETS = sorted(product((-1, 0, 1), repeat=2), key=itemgetter(1))
-
-def add_points(a, b):
-    return a[0] + b[0], a[1] + b[1]
-
-def neighbors(
-    center,
-    num_directions=8,
-    *,
-    max_size = None,
-    max_x_size = None,
-    max_y_size = None,
-    diagonals=False,
-):
-    assert num_directions in {4, 8, 9}
-    if diagonals:
-        assert num_directions == 4, "diagonals can only be used in 4-directional mode"
-
-    # one or the other
-    if max_size:
-        assert not (max_x_size or max_y_size), "specify only max_size OR max_DIM_size"
-    if max_x_size or max_y_size:
-        assert not max_size, "specify only max_size OR max_DIM_size"
-
-    is_bounded = max_size or max_x_size or max_y_size
-
-    for offset_x, offset_y in OFFSETS:
-        if diagonals and not (offset_x and offset_y):
-            continue
-
-        if num_directions == 4 and not diagonals and offset_x and offset_y:
-            # diagonal; skip
-            continue
-
-        if num_directions != 9 and not (offset_x or offset_y):
-            # skip self
-            continue
-
-        next_x, next_y = add_points(center, (offset_x, offset_y))
-
-        if is_bounded and (next_x < 0 or next_y < 0):
-            continue
-
-        if max_size and (next_x > max_size or next_y > max_size):
-            continue
-
-        if max_x_size and (next_x > max_x_size):
-            continue
-
-        if max_y_size and (next_y > max_y_size):
-            continue
-
-        yield (next_x, next_y)
+cardinals = [
+    (0, 1),
+    (0, -1),
+    (1, 0),
+    (-1, 0),
+]
 
 def parse_grid(raw_grid, ignore_chars):
     result = {}
@@ -82,7 +33,7 @@ with open('input.txt') as input_file:
         [(0, next(k for k, v in grid.items() if v == "S"))]
     )
 
-    def num_points_where(f) -> int:
+    def num_points_where(f):
         return sum(f(v) for v in visited.values())
 
     while queue:
@@ -92,8 +43,9 @@ with open('input.txt') as input_file:
             continue
 
         visited[point] = distance
+        px, py = point
 
-        for n in neighbors(point, num_directions=4):
+        for n in [(px + ox, py + oy) for ox, oy in cardinals]:
             if n in visited or n not in plot_locations:
                 continue
 
@@ -103,8 +55,6 @@ with open('input.txt') as input_file:
     assert (
         distance_to_edge == 65
     ), f"unexpected distance to edge, got {distance_to_edge}"
-
-    part_1 = num_points_where(lambda v: v < distance_to_edge and v % 2 == 0)
 
     n = (26501365 - distance_to_edge) // grid_size
     assert n == 202300, f"n calc wrong, got {n}"
