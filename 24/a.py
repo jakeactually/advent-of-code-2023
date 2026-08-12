b9490ff5d938b4fc33b36b060b9870f7ccbfc0fe
Jeyko Caicedo <jakeactually@outlook.com>
2026-08-12 16:22:27 -0500

    day 24 a

--- 24/a.py
+++ 24/a.py
@@ -2,10 +2,7 @@ import re
 from typing import Self
 
 class Point2D:
-    x: float
-    y: float
-
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
@@ -25,20 +22,15 @@ class Point2D:
         return f"Point2D(x={self.x}, y={self.y})"
 
 class Ray2D:
-    origin: Point2D
-    direction: Point2D
-
-    def __init__(self, origin, direction):
-            self.origin = origin
-            self.direction = direction
-
-    def distance_to_point(self, p: Point2D) -> float:
-        t = self.project_point(p)
-        closest = self.point_at(t)
-        return p.distance_to(closest)
-    
+    def __init__(self, pos: Point2D, vel: Point2D):
+            self.pos = pos
+            self.vel = vel
+            self.a = vel.y
+            self.b = -vel.x
+            self.c = vel.y * pos.x - vel.x * pos.y
+        
     def __repr__(self) -> str:
-            return f"Ray2D(origin={self.origin}, direction={self.direction})"
+            return f"Ray2D(pos={self.pos}, vel={self.vel})"
 
 with open('input.txt') as input:
     rays = []
@@ -50,22 +42,24 @@ with open('input.txt') as input:
         ray = Ray2D(a, b)
         rays.append(ray)
 
+    total = 0
+
     for i in range(len(rays)):
         for j in range(i + 1, len(rays)):
-            a = rays[i]
-            b = rays[j]
+            r1 = rays[i]
+            r2 = rays[j]
             
-            d = b.origin - a.origin
-            det = b.direction.dot(a.direction)
-
-            if det == 0:
-                print("never")
+            if r1.a * r2.b == r1.b * r2.a:
                 continue
 
-            u = d.dot(b.direction) / det
-            v = d.dot(a.direction) / det
+            x = (r1.c * r2.b - r2.c * r1.b) / (r1.a * r2.b - r2.a * r1.b)
+            y = (r2.c * r1.a - r1.c * r2.a) / (r1.a * r2.b - r2.a * r1.b)
 
-            p1 = a.origin + a.direction * u
-            p2 = b.origin + b.direction * v
+            low = 200000000000000
+            high = 400000000000000
 
-            print(p1, p2)
+            if low <= x <= high and low <= y <= high:
+                if all((x - hs.pos.x) * hs.vel.x >= 0 and (y - hs.pos.y) * hs.vel.y >= 0 for hs in (r1, r2)):
+                    total += 1
+
+    print(total)
