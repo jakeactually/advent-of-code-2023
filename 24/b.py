05f3da1bf8c070c22e30290cd14c1f05a2b469a8
Jeyko Caicedo <jakeactually@outlook.com>
2026-08-12 17:49:33 -0500

    day 24 b

--- 24/b.py
+++ 24/b.py
@@ -1,4 +1,16 @@
-import re
+# https://www.youtube.com/watch?v=guOyA7Ijqgk
 
-with open('input.txt') as input:
-    print(input)
+import sympy
+
+hailstones = [tuple(map(int, line.replace("@", ",").split(","))) for line in open('input.txt')]
+
+xn, yn, zn, vxn, vyn, vzn = sympy.symbols("xn yn zn vxn vyn vzn")
+
+equations = []
+
+for sx, sy, sz, vx, vy, vz in hailstones:
+    equations.append((xn - sx) * (vy - vyn) - (yn - sy) * (vx - vxn))
+    equations.append((yn - sy) * (vz - vzn) - (zn - sz) * (vy - vyn))
+
+answers = sympy.solve(equations)
+print(answers)
